da1ff837f5ef7a80de92696748b8818443c8f269
juan_777 <jortega27@unisalle.edu.co>
2022-07-23 17:00:04 -0500

    first commit

---
+++ src/main/java/com/fundamentosplatzi/springboot/funadamentos/FunadamentosApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.funadamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunadamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunadamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosplatzi/springboot/funadamentos/FunadamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.funadamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunadamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
